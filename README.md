3d6428ba589edb5a9635edce42b65e89ef4358c6
KyleGamez12 <kylesworldapplications@gmail.com>
2021-02-24 17:26:11 +0000

    Done the report command

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ You will have to install a few requirements
 [NodeJS](https://nodejs.org)
 [Visual Studio Code](https://code.visualstudio.com)
 Install the packages using `npm install`
+Go to the Path ../structres/client.js and go to `this.prefix =` And set your prefix.
+Go to the index.js and Delete the `YOUR TOKEN HERE` 
 # `Commands`
 `ping` - Bot Latency.
+`report` - Reports a user from your Guild.
--- commands/report.js
+++ commands/report.js
@@ -1,5 +1,6 @@
 const Client = require('../structres/client');
 const { MessageEmbed } = require('discord.js')
+const color = require('../color.json');
 module.exports = {
   name: "report",
       /**
@@ -11,7 +12,7 @@ module.exports = {
     let user = message.mentions.members.first()
     if (!user) {
       return message.channel.send(
-        `>>> 🔬| I didnt found a user called ${args[0]}.`
+        `>>> 🔬| I didnt find a user called ${args[0]}.`
 
       );
      if (message.mentions.users.first().bot) {
@@ -22,8 +23,20 @@ module.exports = {
       return message.channel.send("You can not warn yourself");
     }
    if (message.mentions.users.first() === message.guild.owner.id) {
-
+   return message.channel.send(`👑| ${args[0]} Is the owner of ${message.guild.name}.`);
    }
+   let reportReason = args(1).join(" ");
+   if (!reportReason) {
+     message.channel.send(`📜| You must porvide a reason for ${args[0]}'s report.`)
+   }
+   const reportSucess = new MessageEmbed()
+   .setTitle(`${args[0]}'s Report Has been sent.`)
+   .setDescripton(`We have recived your report for ${message.guild.name}. Moderators are on their way.`)
+   .addField(`Reason:`, `${reason}`)
+   .addField("Target User", `${args[0]}`)
+   .setFooter("Make sure to send Evidence!")
+   .setColor("LIGHTBLUE")
+   message.reply(reportSucess)
     }
  }
 }
